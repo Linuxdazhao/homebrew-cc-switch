451a5ab28d9b19462b54c75c82ff5d6c075c278a
zhaojing.jz <zhaojing.jz@alibaba-inc.com>
2026-05-18 15:54:15 +0800

    Update cc-switch to v0.1.24

--- Formula/cc-switch.rb
+++ Formula/cc-switch.rb
@@ -1,24 +1,24 @@
 class CcSwitch < Formula
   desc "A CLI tool for managing multiple Claude API configurations and automatically switching between them"
   homepage "https://github.com/Linuxdazhao/cc_auto_switch"
-  version "0.1.19"
+  version "0.1.24"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-aarch64-apple-darwin.tar.gz"
-      sha256 "fffffdb06fc06f785cea5cd551e7ae813d3f513e45c4acb9732ddb5f359ff202"
+      sha256 "26d9f2d5d11e0f79a18b1b8136918ba451e7e7ccc19344aeeafc78623b0778a3"
     else
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-x86_64-apple-darwin.tar.gz"
-      sha256 "82026818e2fce01945183ab4c4c95814375fe8f200cd7638a55fbfd3b6403c57"
+      sha256 "31209dc63e190eca9a4a14471759c39952684018f1b7132fbecc41ce26b9bc57"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d086f12bed9ebe815c60ee7983e8b0f3a844be82b8fb800056da18bfa125fa35"
+      sha256 "7f8b9c336c5d5179a032bc6276fb80d228c6c64d437d11826c25aa0d70c0f25c"
     else
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "60e60bcf2f98baa6966057a787455401d80832b40b89941cf47bfb2d9db45eae"
+      sha256 "a27c20914709dc4cf14343c868261e0491a21226b1659e33466bcbb599378c8a"
     end
   end
 
@@ -28,7 +28,7 @@ class CcSwitch < Formula
 
   test do
     assert_match "cc-switch", shell_output("#{bin}/cc-switch --help")
-    assert_match version.to_s, shell_output("#{bin}/cc-switch --version")
+    assert_match version.to_s, shell_output("#{bin}/cc-switch version")
   end
 
   def caveats
