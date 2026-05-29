2ff88d67176d923559789a3d9e226dd31a5229c2
钊  景 <jingzhaolinux@163.com>
2026-05-29 10:33:52 +0800

    chore: bump cc-switch to 0.1.37

--- Formula/cc-switch.rb
+++ Formula/cc-switch.rb
@@ -1,24 +1,24 @@
 class CcSwitch < Formula
   desc "A CLI tool for managing multiple Claude API configurations and automatically switching between them"
   homepage "https://github.com/Linuxdazhao/cc_auto_switch"
-  version "0.1.36"
+  version "0.1.37"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-aarch64-apple-darwin.tar.gz"
-      sha256 "8dee78037ccdef8fb4ccc201255f554c2e345f21637df23e44af7966b3ca10ec"
+      sha256 "4d201bb5fad13fda21dd2796ca893edcb2e45c83cfc26738682928e980823a67"
     else
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-x86_64-apple-darwin.tar.gz"
-      sha256 "f6259d593ab5942f60886597a4227887e3f04e12f6bfff11795443019a56ecb9"
+      sha256 "e2e97a668f5ac6e0092e5d2d32a2a74d2c97572b9acb80ef3304d9a5070b6631"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "1045ce635035086680a2577aaf61030675a6e865afb0d5e467ca64f41559fac7"
+      sha256 "711563fd8d6126d80bc74d4c754147017d87a529709ff30006c7099e05f5cdb6"
     else
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "5d95bbb2f8781a366b0491c75b243d9c500fe47ca356bf8c0e179c550bfbf98c"
+      sha256 "a6e3d1466b185657516aceca4604c1cddaca1f3e55eb8ddb7e6e8d793f906cfa"
     end
   end
 
