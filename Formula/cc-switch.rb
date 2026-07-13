7e50971e155d91272a0752b60a9f29184575d557
钊  景 <jingzhaolinux@163.com>
2026-07-13 20:06:39 +0800

    chore: bump cc-switch to 0.2.1

--- Formula/cc-switch.rb
+++ Formula/cc-switch.rb
@@ -1,24 +1,24 @@
 class CcSwitch < Formula
   desc "A CLI tool for managing multiple Claude API configurations and automatically switching between them"
   homepage "https://github.com/Linuxdazhao/cc_auto_switch"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-aarch64-apple-darwin.tar.gz"
-      sha256 "2b1625c0d1f94b42df552d11a83fd6bf06b7741168596a86e75b9dcd3d30db2e"
+      sha256 "670686b53ef1c08227268244676b60347a2a83b449da38178f521bf042c5cf85"
     else
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-x86_64-apple-darwin.tar.gz"
-      sha256 "668306285c012424fcf57560153d4e2cc3770d67c9729d679c24ab4d90cf5bcc"
+      sha256 "fc62737bbccaf28ea4624723a1ccee65c8bd267f2ca3c72272e71e34557f5a21"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "661721091c527bcc12a91d82a17789b87d63e11ef49f33b18dc5fe971c50923a"
+      sha256 "8b4aaddce42c4790812aa1e980d0500771fc55a4e21a9d8904ed0e88c30706f2"
     else
       url "https://github.com/Linuxdazhao/cc_auto_switch/releases/download/v#{version}/cc-switch-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8ce22303777ed9e556d3d982c3fe31561c5735f4f009c04e82b7d45f9242258b"
+      sha256 "2f2fffa36f73834e4077acba56562e9d2e1c0e4e41a9dcc55e6b0fc964130217"
     end
   end
 
@@ -36,23 +36,23 @@ class CcSwitch < Formula
       To use cc-switch effectively:
 
       1. Add configurations:
-         cc-switch add my-config TOKEN_HERE https://api.anthropic.com
+ cc-switch add my-config TOKEN_HERE https://api.anthropic.com
 
       2. Switch between configurations:
-         cc-switch use my-config
+ cc-switch use my-config
 
       3. Interactive mode:
-         cc-switch current
+ cc-switch current
 
       4. Shell completion:
-         # For fish
-         cc-switch completion fish > ~/.config/fish/completions/cc-switch.fish
+ # For fish
+ cc-switch completion fish > ~/.config/fish/completions/cc-switch.fish
 
-         # For zsh
-         cc-switch completion zsh > ~/.zsh/completions/_cc-switch
+ # For zsh
+ cc-switch completion zsh > ~/.zsh/completions/_cc-switch
 
-         # For bash
-         cc-switch completion bash > ~/.bash_completion.d/cc-switch
+ # For bash
+ cc-switch completion bash > ~/.bash_completion.d/cc-switch
 
       For more information, visit: https://github.com/Linuxdazhao/cc_auto_switch
     EOS
